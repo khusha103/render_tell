eabed7eca3425c779cd5f62e0a5d470a453f7635
KhushaEkarigar <khushboo.sharma@ekarigar.com>
2025-06-14 18:03:06 +0530

    prototype message apis

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -5,6 +5,6 @@ const chatController = require('../controllers/chatController');
 router.post('/send', chatController.sendMessage);
 router.get('/messages', chatController.getMessages);
 // GET /api/chats/prototype/get-private-messages
-router.get('/prototype/get-private-messages', chatController.getPrivateMessages);
+// router.get('/prototype/get-private-messages', chatController.getPrivateMessages);
 
 module.exports = router;
